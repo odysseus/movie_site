2a2fe097fb7a50a7ee32b1c905ec79a877d33f6a
Ryan Case <rmcase@gmail.com>
2015-10-23 17:29:48 -0600

    Initial commit

---
+++ media.py
@@ -0,0 +1,10 @@
+# Needs:
+# Movie Title
+# Box/Poster Art
+# Trailer
+
+class Movie(object):
+    def __init__(self):
+        self.title = None
+        self.poster_image_url = None
+        self.trailer_youtube_url = None
